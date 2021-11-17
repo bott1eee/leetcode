07f064fd0f9ff95d16feeaf23e385b1fcef40df3
ryuyoung <yeoyoungmirei@gmail.com>
2021-11-17 10:59:08 +0800

    94-二叉树的中序遍历（迭代法）

--- src/biTree/InOrderTraversal.java
+++ src/biTree/InOrderTraversal.java
@@ -2,9 +2,14 @@ package biTree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+import java.util.TreeMap;
 
 public class InOrderTraversal {
 
+    /*
+    递归法
+     */
     public List<Integer> inorderTraversal(TreeNode root) {
 
         List<Integer> result = new ArrayList<>();
@@ -20,5 +25,29 @@ public class InOrderTraversal {
         traversal(root.left, result);
         result.add(root.val);
         traversal(root.right, result);
+    }
+
+    /*
+    迭代法
+     */
+    public List<Integer> inorderTraversal_2(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+        if (root == null)
+            return result;
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty()) {
+            if (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }else {
+                cur = stack.pop();
+                result.add(cur.val);
+                cur = cur.right;
+            }
+        }
+        return result;
     }
 }
