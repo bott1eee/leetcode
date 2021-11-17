00016b953d15a0be88599b91382866a0acb83d33
ryuyoung <yeoyoungmirei@gmail.com>
2021-11-17 10:34:13 +0800

    144-二叉树的前序遍历（迭代法）

--- src/biTree/PreOrderTraversal.java
+++ src/biTree/PreOrderTraversal.java
@@ -1,11 +1,17 @@
 package biTree;
 
 
+import sun.reflect.generics.tree.Tree;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class PreOrderTraversal {
 
+    /*
+    递归法
+     */
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         traversal(root, result);
@@ -18,5 +24,27 @@ public class PreOrderTraversal {
         result.add(root.val);
         traversal(root.left, result);
         traversal(root.right, result);
+    }
+
+    /*
+    迭代法
+    使用栈存储结点，先存储右节点再存储左节点
+     */
+    public List<Integer> preOrderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null)
+            return result;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+            if (node.right != null)
+                stack.push(node.right);
+            if (node.left!=null)
+                stack.push(node.left);
+        }
+        return result;
     }
 }
