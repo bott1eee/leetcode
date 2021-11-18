ebdfe8dbfa3d0875b85405d91c9462ac5dcedbb8
ryuyoung <yeoyoungmirei@gmail.com>
2021-11-18 10:58:58 +0800

    199-二叉树的右视图

--- src/biTree/ConvertLevelOrder.java
+++ src/biTree/ConvertLevelOrder.java
@@ -1,2 +1,44 @@
-package biTree;public class ConvertLevelOrder {
+package biTree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class ConvertLevelOrder {
+
+    /*
+    先使用迭代法正序层序遍历二叉树
+    再将结果 list 逆置即可
+     */
+    public List<List<Integer>> convertLevelOrder(TreeNode root) {
+
+        List<List<Integer>> result_temp = new ArrayList<>();
+        if (root == null)
+            return result_temp;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int length = 0;
+        while (!queue.isEmpty()) {
+            List<Integer> item = new ArrayList<>();
+            length = queue.size();
+
+            while (length > 0) {
+                TreeNode node = queue.poll();
+                item.add(node.val);
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+                length--;
+            }
+            result_temp.add(item);
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = result_temp.size(); i >= 0; i--) {
+            result.add(result_temp.get(i));
+        }
+        return result;
+    }
 }
---
+++ src/biTree/RightSideView.java
@@ -0,0 +1,71 @@
+package biTree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class RightSideView {
+
+    /*
+    先对二叉树进行层序遍历，得到 List<List<Integer>> 结果
+    取 List<List<Integer>> 中的每项 List 的最后一个元素，即为最右边视角的元素
+     */
+    public List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+        if (root == null)
+            return result;
+        List<List<Integer>> allResult = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int length = 0;
+        while (!queue.isEmpty()) {
+            List<Integer> item = new ArrayList<>();
+            length = queue.size();
+            while (length > 0) {
+                TreeNode node = queue.poll();
+                item.add(node.val);
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+                length--;
+            }
+            allResult.add(item);
+        }
+        for (List<Integer> list : allResult) {
+            result.add(list.get(list.size() - 1));
+        }
+        return result;
+    }
+
+    /*
+    上述方法对二叉树层序遍历时将所有结点存入结果数组里
+    这里不同的是
+    先将结点的右孩子入队，再将左孩子入队
+    然后对每层循环结束后，将队列的 peek 元素的值存入结果数组里，即为最右视角的值
+     */
+    public List<Integer> rightSideView_2(TreeNode root) {
+
+        List<Integer> result = new ArrayList<>();
+        if (root == null)
+            return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int length = 0;
+        while (!queue.isEmpty()) {
+            length = queue.size();
+            while (length > 0) {
+                TreeNode node = queue.poll();
+                if (node.right != null)
+                    queue.offer(node.right);
+                if (node.left != null)
+                    queue.offer(node.left);
+                length--;
+            }
+            result.add(queue.peek().val);
+        }
+        return result;
+    }
+}
